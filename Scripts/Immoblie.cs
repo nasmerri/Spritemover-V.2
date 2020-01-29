a61a687c8995ec8694ac5ed2c1adfdf91fea679f
Naszir M <47009616+nasmerri@users.noreply.github.com>
2020-01-29 16:48:58 -0700

    Add files via upload

---
+++ Scripts/Immoblie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Immoblie : MonoBehaviour
+{
+	SpriteMover im;
+    // Start is called before the first frame update
+    void Start()
+    {
+		im = GetComponent<SpriteMover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+		{
+			im.enabled = !im.enabled;
+        }
+    }
+}
